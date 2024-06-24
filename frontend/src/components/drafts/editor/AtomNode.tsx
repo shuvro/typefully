725f32b4789cc22a4ba337c6746bb5d6400e037d
shuvro <shuvro.dhrubo@gmail.com>
2024-06-24 18:27:30 +0600

    Refactor AtomNode.tsx code for better readability
    Indentation has been adjusted in multiple lines of the AtomNode.tsx file to improve the readability and maintainability of the code. Minor changes include formatted object destructuring and removing unnecessary space in import statements.

--- frontend/src/components/drafts/editor/AtomNode.tsx
+++ frontend/src/components/drafts/editor/AtomNode.tsx
@@ -1,5 +1,5 @@
 import {MinusIcon, MixerHorizontalIcon, PlusIcon} from "@radix-ui/react-icons";
-import { Node } from "@tiptap/core";
+import {Node} from "@tiptap/core";
 import {
   NodeViewContent,
   NodeViewProps,
@@ -8,7 +8,7 @@ import {
 } from "@tiptap/react";
 import {useCallback, useContext} from "react";
 import cn from "../../../utils/cn";
-import { Button } from "../../ui/button";
+import {Button} from "../../ui/button";
 import {SelectedDraftContext} from "../../../context/SelectedDraftContext.ts";
 import {Draft} from "../../../types.ts";
 
@@ -19,9 +19,9 @@ export const AtomNode = Node.create({
   inline: false,
   draggable: true,
   parseHTML() {
-    return [{ tag: "atom" }];
+    return [{tag: "atom"}];
   },
-  renderHTML({ HTMLAttributes }) {
+  renderHTML({HTMLAttributes}) {
     return ["atom", HTMLAttributes, 0];
   },
   addNodeView() {
@@ -30,8 +30,8 @@ export const AtomNode = Node.create({
 });
 
 const NodeView = (nodeViewProps: NodeViewProps) => {
-  const { editor, node, getPos, deleteNode } = nodeViewProps;
-  const { selectedDraftId } = useContext(SelectedDraftContext);
+  const {editor, node, getPos, deleteNode} = nodeViewProps;
+  const {selectedDraftId} = useContext(SelectedDraftContext);
 
   const charCount = editor.state.doc.textBetween(
     getPos(),
@@ -62,51 +62,51 @@ const NodeView = (nodeViewProps: NodeViewProps) => {
   }, [deleteNode, editor.commands]);
 
   const handleSplitText = useCallback(async () => {
-   const res = await fetch("http://localhost:8000/drafts/split-text/", {
-       method: "POST",
-        body: JSON.stringify({
-            draft_id: selectedDraftId,
-        }),
-        headers: {
-            "Content-Type": "application/json",
-        }
+    const res = await fetch("http://localhost:8000/drafts/split-text/", {
+      method: "POST",
+      body: JSON.stringify({
+        draft_id: selectedDraftId,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      }
     });
 
-        const updatedDraft = await res.json().then((data) => {
-            return data.draft as Draft;
-        });
+    const updatedDraft = await res.json().then((data) => {
+      return data.draft as Draft;
+    });
 
-        // Check if tweets array is empty
-        if (!updatedDraft.tweets || updatedDraft.tweets.length === 0) {
-            console.log('No tweets to process');
-            return; // Return early from the function
-        }
-        deleteNode(); // Delete the current node
+    // Check if tweets array is empty
+    if (!updatedDraft.tweets || updatedDraft.tweets.length === 0) {
+      console.log('No tweets to process');
+      return; // Return early from the function
+    }
+    deleteNode(); // Delete the current node
 
-        updatedDraft.tweets.forEach((tweet) => {
-            editor
-                .chain()
-                .insertContentAt(getPos(), {
-                    type: "atom",
-                    content: [
-                        {
-                            type: "paragraph",
-                            content: [
-                                {
-                                    type: "text",
-                                    text: tweet,
-                                },
-                            ],
-                        },
-                    ],
-                })
-                .run();
-        });
+    updatedDraft.tweets.forEach((tweet) => {
+      editor
+        .chain()
+        .insertContentAt(getPos(), {
+          type: "atom",
+          content: [
+            {
+              type: "paragraph",
+              content: [
+                {
+                  type: "text",
+                  text: tweet,
+                },
+              ],
+            },
+          ],
+        })
+        .run();
+    });
   }, [deleteNode, editor, getPos, selectedDraftId]);
 
   return (
     <NodeViewWrapper>
-      <NodeViewContent />
+      <NodeViewContent/>
       {/* this wrapping div prevents selection of children on cmd+a */}
       <div contentEditable={false} className="[&>*]:select-none">
         <span
@@ -122,7 +122,7 @@ const NodeView = (nodeViewProps: NodeViewProps) => {
           className="mt-2 ml-3"
           variant="secondary"
           onClick={handleAddAtom}
-          icon={<PlusIcon />}
+          icon={<PlusIcon/>}
           tabIndex={-1}
         />
         <Button
@@ -131,9 +131,9 @@ const NodeView = (nodeViewProps: NodeViewProps) => {
           variant="secondary"
           onClick={handleRemoteAtom}
           tabIndex={-1}
-          icon={<MinusIcon />}
+          icon={<MinusIcon/>}
         />
-        <div className="h-[1px] bg-slate-200 w-full mt-2 mb-3" />
+        <div className="h-[1px] bg-slate-200 w-full mt-2 mb-3"/>
       </div>
       {charCount > 280 &&
         <div className="flex flex-row max-w-full items-center flex-wrap justify-center gap-3">
